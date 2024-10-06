bd84ed764bf9b46ebfde9b9f004fe9e45c5328c8
Kaique Vieira <kvieiramiranda@gmail.com>
2024-10-06 16:12:09 -0300

    initial commit

---
+++ src/main/java/com/academia/Academia.java
@@ -0,0 +1,13 @@
+package com.academia;
+
+/**
+ *
+ * @author Kaique Vieria
+ * @author Kayky Nery
+ * @version 1.0
+ */
+public class Academia {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
