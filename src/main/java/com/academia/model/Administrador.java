2d7a9a61e89c751f76318eb47b56a868911699c6
KaykyNery <kaykynav@gmail.com>
2024-10-08 00:26:07 -0300

    Classe administrador

---
+++ src/main/java/com/academia/model/Administrador.java
@@ -0,0 +1,14 @@
+package com.academia.model;
+
+public class Administrador extends Funcionario {
+
+    public Administrador(String nome, String cpf, String usuario, String senha) {
+        super(nome, cpf, usuario, senha);
+    }
+    
+@Override
+    public String toString() {
+        return "Administrador [Nome: " + getNome() + ", CPF: " + getCpf() + 
+               ", Usuário: " + getUsuario() + "]";
+    }
+}
--- src/main/java/com/academia/model/Sala.java
+++ src/main/java/com/academia/model/Sala.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.academia.model;
 
-/**
- *
- * @author kayky
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sala {
+    private int id;
+    private int capacidade;
+    private String modalidade;
+
+    private static List<Sala> salas = new ArrayList<>();
+
+    public Sala(int id, int capacidade, String modalidade) {
+        this.id = id;
+        this.capacidade = capacidade;
+        this.modalidade = modalidade;
+   
+        salas.add(this);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public String getModalidade() {
+        return modalidade;
+    }
     
+    public static List<Sala> getSalas() {
+        return salas;
+    }
+
+    @Override
+    public String toString() {
+        return "Sala{" + "id=" + id + ", capacidade=" + capacidade +
+                ", modalidade='" + modalidade + '\'' + '}';
+    }
 }
