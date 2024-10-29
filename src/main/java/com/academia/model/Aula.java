c90576509b2c2b4b5c6426a88dc0ebfe102f18dc
Kaique Miranda <kaique.miranda@nuria.com.br>
2024-10-29 10:36:48 -0300

    Implementação da interface AulaComponente
    Foi feita uma implementação da interface AulaComponent na classe Aula.

--- src/main/java/com/academia/model/Aula.java
+++ src/main/java/com/academia/model/Aula.java
@@ -1,19 +1,21 @@
 package com.academia.model;
 
 
-public class Aula {
+public class Aula implements AulaComponent {
     private String id;
     private String nome;
     private Professor professor;
     private String horario;
     private int vagasDisponiveis;
-
-    public Aula(String id, String nome, Professor professor, String horario, int vagasDisponiveis) {
+    private float preco;
+     
+    public Aula(String id, String nome, Professor professor, String horario, int vagasDisponiveis, float preco) {
         this.id = id;
         this.nome = nome;
         this.professor = professor;
         this.horario = horario;
         this.vagasDisponiveis = vagasDisponiveis;
+        this.preco = preco;
     }
 
     public String getId() {
@@ -32,6 +34,7 @@ public class Aula {
         this.nome = nome;
     }
 
+    @Override
     public Professor getProfessor() {
         return professor;
     }
@@ -56,22 +59,38 @@ public class Aula {
         this.vagasDisponiveis = vagasDisponiveis;
     }
 
+    @Override
+    public float getPreco() {
+        return this.preco;
+    }
+    
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+    @Override
     public void agendarAluno(Aluno aluno) {
         if (vagasDisponiveis > 0) {
             vagasDisponiveis--;
-            System.out.println("Aluno " + aluno.getNome() + " agendado na aula.");
+            System.out.println("Aluno " + aluno.getNome() + " agendado na aula " + this.nome);
         } else {
             System.out.println("Não há vagas disponíveis.");
         }
     }
 
+    @Override
     public void cancelarInscricao(Aluno aluno) {
         vagasDisponiveis++;
-        System.out.println("Inscrição do aluno " + aluno.getNome() + " cancelada.");
+        System.out.println("Inscrição do aluno " + aluno.getNome() + " cancelada na aula " + this.nome);
     }
 
     public int verificarVagas() {
         return vagasDisponiveis;
+    }
+    
+    @Override
+    public void exibirDetalhes() {
+        System.out.println(this.toString());
     }
 
     @Override
