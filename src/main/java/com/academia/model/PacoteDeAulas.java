35477920e5a0efad2c798e09c3bb248d84acb52f
Kaique Miranda <kaique.miranda@nuria.com.br>
2024-10-29 10:37:32 -0300

    Criação de um composite PacoteDeAulas
    Foi criado um composite PacoteDeAulas com sendo um objeto complexo de aula.

---
+++ src/main/java/com/academia/model/PacoteDeAulas.java
@@ -0,0 +1,52 @@
+package com.academia.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PacoteDeAulas implements AulaComponent {
+    private String nome;
+    private List<AulaComponent> components = new ArrayList<>();
+    
+    public PacoteDeAulas(String nome) {
+        this.nome = nome;
+    }
+    
+    public void adicionarAula(AulaComponent componente) {
+        components.add(componente);
+    }
+    
+    public void removerAula(AulaComponent componente) {
+        components.remove(componente);
+    }
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Pacode de Aulas: " + this.nome);
+        for (AulaComponent componente : components) {
+            componente.exibirDetalhes();
+        }
+    }
+    
+    @Override
+    public float getPreco() {
+        return (float) components.stream().mapToDouble(AulaComponent::getPreco).sum();
+    }
+    
+    @Override
+    public Professor getProfessor() {
+        return null;
+    }
+    
+    @Override
+    public void agendarAluno(Aluno aluno) {
+        for (AulaComponent componente : components) {
+            componente.agendarAluno(aluno);
+        }
+    }
+    
+    @Override
+    public void cancelarInscricao(Aluno aluno) {
+        for (AulaComponent componente : components) {
+            componente.cancelarInscricao(aluno);
+        }
+    }
+}
